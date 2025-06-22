185886a5aa51f068e98048618a2f786549ebf59c
Dominic Kramer <DominicKramer@gmail.com>
2025-06-22 11:18:53 -0700

    snapshot of markdown-view

--- web/src/components/ui/markdown-view.tsx
+++ web/src/components/ui/markdown-view.tsx
@@ -16,6 +16,14 @@ import "prismjs/components/prism-css";
 import "prismjs/components/prism-markdown";
 import styles from "./markdown-view.module.css";
 
+import Markdown from 'react-markdown'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import remarkGfm from 'remark-gfm'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+import 'katex/dist/katex.min.css' // `rehype-katex` does not import the CSS for you
+
 export interface MarkdownViewProps {
     markdown: string;
 }
@@ -42,6 +50,36 @@ export default function MarkdownView({ markdown }: MarkdownViewProps) {
         <div
             className={styles.markdown}
             dangerouslySetInnerHTML={{ __html: htmlContent }}
+        />
+    );
+}
+
+export function MarkdownView2({ markdown }: MarkdownViewProps) {
+    return (
+        <Markdown
+            remarkPlugins={[remarkGfm, remarkMath]}
+            rehypePlugins={[rehypeKatex]}
+            children={markdown}
+            components={{
+            code(props) {
+                const {children, className, node, ...rest} = props
+                const match = /language-(\w+)/.exec(className || '')
+                return match ? (
+                    // @ts-ignore
+                    <SyntaxHighlighter
+                        {...rest}
+                        PreTag="div"
+                        children={String(children).replace(/\n$/, '')}
+                        language={match[1]}
+                        style={materialLight}
+                    />
+                ) : (
+                    <code {...rest} className={className}>
+                        {children}
+                    </code>
+                )
+            }
+            }}
         />
     );
 }
